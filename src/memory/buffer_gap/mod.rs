e8f3296149e894c02edc16055e85aca57e670162
Deepak Sirone <deepaksirone94@gmail.com>
2017-04-04 05:30:55 -0400

    Update memory module

--- src/memory/buffer_gap/mod.rs
+++ src/memory/buffer_gap/mod.rs
@@ -1,2 +1,28 @@
+pub struct BufferList
+{
+    head: Option<Box<Buffer>>,
+}
+
+struct Buffer
+{
+    name: String,
+    nextentry: Option<Box<Buffer>>,
+
+    point: u64,
+    modified: u8,
+    length: u64,
+
+    filename: String,
+    modename: String,
+
+    data: Option<Box<GapBuffer>>
+}
+
+
+struct GapBuffer
+{
+
+
+
 
 
