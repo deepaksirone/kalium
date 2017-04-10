58430db6035a8caa37bb60fab7f20f18733515e0
Deepak Sirone <deepaksirone94@gmail.com>
2017-04-10 08:13:02 -0400

    Update buffer_gap

--- src/memory/buffer_gap/mod.rs
+++ src/memory/buffer_gap/mod.rs
@@ -1,16 +1,18 @@
+const BUFSIZE: usize = 4500; 
+
 pub struct BufferList
 {
-    head: Option<Box<Buffer>>,
+    head: Vec<Box<BufferDescriptor>>,
 }
 
-struct Buffer
+struct BufferDescriptor
 {
     name: String,
     nextentry: Option<Box<Buffer>>,
 
-    point: u64,
-    modified: u8,
-    length: u64,
+    point: usize,
+    modified: usize,
+    length: usize,
 
     filename: String,
     modename: String,
@@ -21,8 +23,50 @@ struct Buffer
 
 struct GapBuffer
 {
+   left: Vec<char>,
+   right: Vec<char>,
     
+   gap_start: usize,
+   gap_end:   usize,
 
+}
+
+impl GapBuffer
+{
+    fn user_to_gap(&self, user_point: usize) -> usize {
+        if user_point <= self.gap_start {
+            user_point
+        }
+        else {
+            (self.gap_end - self.gap_start) + user_point
+        }
+    }
+
+    pub fn new() -> Self {
+        GapBuffer { 
+            left: Vec::new(),
+            right: Vec::new(),
+            gap_start: 0,
+            gap_end: BUFSIZE - 1
+        }
+    }
+
+    fn pop_left() -> Option<char> {
+        unimplemented!();   
+    }
+
+    fn pop_right() -> Option<char> {
+        unimplemented!(); 
+    }
+
+    fn move_left(&mut self) -> bool {
+
+    }
+    
+    fn move_right(&mut self) -> bool {
+
+    }
 
 
 }
+
