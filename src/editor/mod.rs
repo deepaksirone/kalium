9e5678eaa4578712340a40cf482e7d1ca7e4501c
Deepak Sirone <deepaksirone94@gmail.com>
2017-05-28 12:25:47 -0400

    [Update] editor/mod.rs

--- src/editor/mod.rs
+++ src/editor/mod.rs
@@ -5,10 +5,17 @@ use memory::buffer::{BufferList, Buffer, BufferIter};
 struct Editor {
     buf_list: BufferList,
     title: Option<String>,
-        
-
+    cur_buf_idx: usize
 
 }
 
+impl Editor
+{
+    pub fn init()
+    {
+        unimplemented!();
+    }
 
+
+}
 
